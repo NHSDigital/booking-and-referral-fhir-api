f054dc9fa203de8c2599a928c21810e8648ec703
Valentinolucas <valentinolucasfantini@gmail.com>
2021-08-04 09:40:02 +0100

    AMB-795 add basic error handling

--- proxies/live/apiproxy/resources/jsc/setTargetUrl.js
+++ proxies/live/apiproxy/resources/jsc/setTargetUrl.js
@@ -1,5 +1,12 @@
 var queryParamaters = context.getVariable("request.querystring");
 var serviceId = context.getVariable("request.header.service-id");
-var kvmValue = "booking-and-referral-config.id." + serviceId
-var url = context.getVariable(kvmValue)
+var kvmValue = "booking-and-referral-config.id." + serviceId;
+var url = context.getVariable(kvmValue);
+var isError = false;
+
+if(url === null | serviceId === null){
+    isError = true
+}
+
+context.setVariable("isError", isError)
 context.setVariable("target.url", url + "?" + queryParamaters);
