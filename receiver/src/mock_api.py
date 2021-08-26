20449daf6fd8e7aed5ed73e4682226e0ab7820f8
Andre Silva <andre.silva2@nhs.net>
2021-08-26 11:53:26 +0100

    meta endpoint with json reponses

--- receiver/src/mock_api.py
+++ receiver/src/mock_api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, jsonify
 import os
 
 app = Flask(__name__)
@@ -6,10 +6,51 @@ app = Flask(__name__)
 
 @app.route('/meta')
 def meta():
+    global expected_response
     env_name = os.environ['ENV_NAME']
-    expected_response = request.args.get('response-type')
 
-    return Response(env_name, status=expected_response, mimetype='application/json')
+    if 'response-type' in request.headers:
+        expected_response = request.headers['response-type']
+
+    return Response(response_builder(expected_response, env_name), status=expected_response,
+                    mimetype='application/json')
+
+
+def response_builder(http_code_response, env_name):
+    if http_code_response == '200':
+        return '{ "providerName": "%s", "resourceType": "CapabilityStatement", "meta": { "profile": [ ' \
+               '"https://www.hl7.org/fhir/CapabilityStatement" ] }, "fhirVersion": "4.0.1", "status": "active" }' % \
+               env_name
+    elif http_code_response == '400':
+        return '{ "providerName": "%s", "resourceType": "OperationOutcome", "meta": { "profile": [ ' \
+               '"http://hl7.org/fhir/STU3/operationoutcome" ] }, "issue": [ { "severity": "error", "code": "invalid", ' \
+               '"details": { "coding": [ { "system": "https://fhir.nhs.uk/STU3/ValueSet/Spine-ErrorOrWarningCode-1", ' \
+               '"code": "BAD_REQUEST" } ] }, "diagnostics": "Any further internal debug details i.e. stack trace ' \
+               'details etc." } ] } ' % env_name
+    elif http_code_response == '401':
+        return '{ "providerName": "%s", "resourceType": "OperationOutcome", "meta": { "profile": [ ' \
+               '"http://hl7.org/fhir/STU3/operationoutcome" ] }, "issue": [ { "severity": "error", "code": "invalid", ' \
+               '"details": { "coding": [ { "system": "https://fhir.nhs.uk/STU3/ValueSet/Spine-ErrorOrWarningCode-1", ' \
+               '"code": "BAD_REQUEST" } ] }, "diagnostics": "Any further internal debug details i.e. stack trace ' \
+               'details etc." } ] } '% env_name
+    elif http_code_response == '403':
+        return '{"providerName": "%s", "resourceType": "OperationOutcome", "meta": {"profile": [ ' \
+               '"http://hl7.org/fhir/STU3/operationoutcome" ] }, "issue": [ { "severity": "error", "code": "invalid", ' \
+               '"details": { "coding": [ { "system": "https://fhir.nhs.uk/STU3/ValueSet/Spine-ErrorOrWarningCode-1", ' \
+               '"code": "BAD_REQUEST" } ] }, "diagnostics": "Any further internal debug details i.e. stack trace ' \
+               'details etc." } ] } ' % env_name
+    elif http_code_response == '404':
+        return '{"providerName": "%s", "resourceType": "OperationOutcome", "meta": {"profile": [ ' \
+               '"http://hl7.org/fhir/STU3/operationoutcome" ] }, "issue": [ { "severity": "error", "code": "invalid", ' \
+               '"details": { "coding": [ { "system": "https://fhir.nhs.uk/STU3/ValueSet/Spine-ErrorOrWarningCode-1", ' \
+               '"code": "BAD_REQUEST" } ] }, "diagnostics": "Any further internal debug details i.e. stack trace ' \
+               'details etc." } ] } ' % env_name
+    elif http_code_response == '500':
+        return '{"providerName": "%s", "resourceType": "OperationOutcome", "meta": {"profile": [ ' \
+               '"http://hl7.org/fhir/STU3/operationoutcome" ] }, "issue": [ { "severity": "error", "code": "invalid", ' \
+               '"details": { "coding": [ { "system": "https://fhir.nhs.uk/STU3/ValueSet/Spine-ErrorOrWarningCode-1", ' \
+               '"code": "BAD_REQUEST" } ] }, "diagnostics": "Any further internal debug details i.e. stack trace ' \
+               'details etc." } ] } ' % env_name
 
 
 if __name__ == "__main__":
