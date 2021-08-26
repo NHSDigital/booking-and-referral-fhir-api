ba7ec4d714aa3b83a991dff4872fb16231c6a84f
Valentinolucas <valentinolucasfantini@gmail.com>
2021-08-26 13:25:02 +0100

    AMB-809 improve setTargetUrl.js

--- proxies/live/apiproxy/resources/jsc/setTargetUrl.js
+++ proxies/live/apiproxy/resources/jsc/setTargetUrl.js
@@ -1,12 +1,34 @@
+
+function get_url(serviceId, endpoint) {
+  /*
+    Get a url value from the booking-and-referral kvm.
+    the valid endpoints at the moment are:
+    - meta
+    - slots
+    
+    If there is no value in the kvm it returns null
+  */
+    var location = "booking-and-referral-config.NHSD-ServiceIdentifier." + serviceId + endpoint;
+    var url = context.getVariable(location);
+    return url
+}
+
+//////////////////////////////////
+// Geting context variables...
+//////////////////////////////////
+
 var queryParamaters = context.getVariable("request.querystring");
-var serviceId = context.getVariable("request.header.NHSD-ServiceIdentifie");
-var kvmValue = "booking-and-referral-config.id." + serviceId;
-var url = context.getVariable(kvmValue);
+var serviceId = context.getVariable("request.header.NHSD-ServiceIdentifier");
+var endpoint = context.getVariable("proxy.pathsuffix")
+
 var isError = false;
+
+var targetUrl = get_url(serviceId,endpoint)
 
 if(url === null | serviceId === null){
     isError = true
 }
 
 context.setVariable("isError", isError)
-context.setVariable("target.url", url + "?" + queryParamaters);
+context.setVariable("target.url", targetUrl + "?" + queryParamaters);
+
