15d7f34779e25cb1879be26cea32dfc7f5a4c135
Valentinolucas <valentinolucasfantini@gmail.com>
2021-09-06 12:27:03 +0100

    AMB-811 contruct target url with path suffix and query parameters

--- proxies/live/apiproxy/resources/jsc/SetTargetUrl.js
+++ proxies/live/apiproxy/resources/jsc/SetTargetUrl.js
@@ -1,15 +1,17 @@
-
+// Get context variables
 var serviceId = context.getVariable("request.header.NHSD-ServiceIdentifier");
 var pathSuffix = context.getVariable("proxy.pathsuffix")
+var queryString = context.getVariable("request.querystring")
 var isError = false;
 
+// Get target url from KVM
 var endpoint = get_endpoint_from_pathsuffix(pathSuffix)
 var targetUrl = get_target_url_from_kvm(serviceId,endpoint)
-
 if(targetUrl === null | serviceId === null){
     isError = true
 }
-
 context.setVariable("isError", isError)
-context.setVariable("target.url", targetUrl);
+
+// Override target url
+context.setVariable("target.url", targetUrl + pathSuffix + "?" + queryString);
 
