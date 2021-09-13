2284956c7be6a74f2ad4d86546f3843ecde0b567
Jalal Hosseini <jalalhosseiny@gmail.com>
2021-09-13 14:16:59 +0100

    AMB-836 add /$process-message endpoint

---
+++ tests/test_process_message.py
@@ -0,0 +1,28 @@
+import pytest
+import requests
+from .configuration import config
+from assertpy import assert_that
+from .example_loader import load_example
+
+
+class TestProcessMessage:
+    @pytest.mark.process_message
+    @pytest.mark.integration
+    def test_create_process_message(self, get_token_client_credentials):
+        # Given
+        token = get_token_client_credentials["access_token"]
+        expected_status_code = 200
+        expected_body = load_example("process_message/POST-success.json")
+
+        # When
+        response = requests.get(
+            url=f"{config.BASE_URL}/{config.BASE_PATH}/$process-message",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "NHSD-ServiceIdentifier": "NHS0001",
+            },
+        )
+
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
+        assert_that(expected_body).is_equal_to(response.json())
