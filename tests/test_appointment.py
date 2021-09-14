0e942ede4363cfb0c47993cd290b9493ec59d8b0
Jalal Hosseini <jalalhosseiny@gmail.com>
2021-09-14 16:07:00 +0100

    AMB-840 update tests with error scenarios

--- tests/test_appointment.py
+++ tests/test_appointment.py
@@ -15,6 +15,29 @@ class TestAppointment:
         token = get_token_client_credentials["access_token"]
         expected_status_code = 200
         expected_body = load_example("appointment/GET-success.json")
+        patient_id = "4857773456"
+
+        # When
+        response = requests.get(
+            url=f"{config.BASE_URL}/{config.BASE_PATH}/Appointment",
+            params={'patientIdentifier': patient_id},
+            headers={
+                "Authorization": f"Bearer {token}",
+                "NHSD-ServiceIdentifier": "NHS0001",
+            },
+        )
+
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
+        assert_that(expected_body).is_equal_to(response.json())
+
+    @pytest.mark.appointment
+    @pytest.mark.integration
+    def test_get_appointments_missing_param_patient_id(self, get_token_client_credentials):
+        # Given
+        token = get_token_client_credentials["access_token"]
+        expected_status_code = 400
+        expected_body = load_example("bad-request.json")
 
         # When
         response = requests.get(
@@ -24,6 +47,7 @@ class TestAppointment:
                 "NHSD-ServiceIdentifier": "NHS0001",
             },
         )
+        print(response.text)
 
         # Then
         assert_that(expected_status_code).is_equal_to(response.status_code)
@@ -52,15 +76,16 @@ class TestAppointment:
 
     @pytest.mark.appointment
     @pytest.mark.integration
-    def test_get_appointment_uuid(self, get_token_client_credentials):
+    def test_get_appointment_bad_id(self, get_token_client_credentials):
         # Given
         token = get_token_client_credentials["access_token"]
-        expected_status_code = 200
-        expected_body = load_example("appointment/id/GET-success.json")
+        expected_status_code = 400
+        expected_body = load_example("bad-request.json")
+        bad_id = "non-uuid"
 
         # When
         response = requests.get(
-            url=f"{config.BASE_URL}/{config.BASE_PATH}/Appointment/{self.existing_appointment_id}",
+            url=f"{config.BASE_URL}/{config.BASE_PATH}/Appointment/{bad_id}",
             headers={
                 "Authorization": f"Bearer {token}",
                 "NHSD-ServiceIdentifier": "NHS0001",
@@ -77,11 +102,12 @@ class TestAppointment:
         # Given
         token = get_token_client_credentials["access_token"]
         expected_status_code = 201
-        expected_body = load_example("appointment/POST-success.txt")
+        expected_res_body = load_example("appointment/POST-success.txt")
 
         # When
         response = requests.post(
             url=f"{config.BASE_URL}/{config.BASE_PATH}/Appointment",
+            json=load_example("appointment/POST-body.json"),
             headers={
                 "Authorization": f"Bearer {token}",
                 "NHSD-ServiceIdentifier": "NHS0001",
@@ -92,7 +118,7 @@ class TestAppointment:
         assert_that(expected_status_code).is_equal_to(response.status_code)
 
         actual_content = response.content.decode('utf-8').strip()
-        assert_that(expected_body).is_equal_to(actual_content)
+        assert_that(expected_res_body).is_equal_to(actual_content)
 
     @pytest.mark.appointment
     @pytest.mark.integration
@@ -100,11 +126,12 @@ class TestAppointment:
         # Given
         token = get_token_client_credentials["access_token"]
         expected_status_code = 200
-        expected_body = ""
+        expected_body = "\"\""
 
         # When
         response = requests.put(
             url=f"{config.BASE_URL}/{config.BASE_PATH}/Appointment/{self.existing_appointment_id}",
+            json=load_example("appointment/id/PUT-body.json"),
             headers={
                 "Authorization": f"Bearer {token}",
                 "NHSD-ServiceIdentifier": "NHS0001",
@@ -121,11 +148,12 @@ class TestAppointment:
         # Given
         token = get_token_client_credentials["access_token"]
         expected_status_code = 200
-        expected_body = ""
+        expected_body = "\"\""
 
         # When
         response = requests.patch(
             url=f"{config.BASE_URL}/{config.BASE_PATH}/Appointment/{self.existing_appointment_id}",
+            json=load_example("appointment/id/PATCH-body.json"),
             headers={
                 "Authorization": f"Bearer {token}",
                 "NHSD-ServiceIdentifier": "NHS0001",
@@ -142,7 +170,7 @@ class TestAppointment:
         # Given
         token = get_token_client_credentials["access_token"]
         expected_status_code = 200
-        expected_body = ""
+        expected_body = "\"\""
 
         # When
         response = requests.delete(
@@ -156,3 +184,71 @@ class TestAppointment:
         # Then
         assert_that(expected_status_code).is_equal_to(response.status_code)
         assert_that(expected_body).is_equal_to(response.content.decode('utf-8'))
+
+    @pytest.mark.appointment
+    @pytest.mark.integration
+    def test_put_appointment_bad_id(self, get_token_client_credentials):
+        # Given
+        token = get_token_client_credentials["access_token"]
+        expected_status_code = 400
+        expected_body = load_example("bad-request.json")
+        bad_id = "non-uuid"
+
+        # When
+        response = requests.put(
+            url=f"{config.BASE_URL}/{config.BASE_PATH}/Appointment/{bad_id}",
+            json=load_example("appointment/id/PUT-body.json"),
+            headers={
+                "Authorization": f"Bearer {token}",
+                "NHSD-ServiceIdentifier": "NHS0001",
+            },
+        )
+
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
+        assert_that(expected_body).is_equal_to(response.json())
+
+    @pytest.mark.appointment
+    @pytest.mark.integration
+    def test_patch_appointment_bad_id(self, get_token_client_credentials):
+        # Given
+        token = get_token_client_credentials["access_token"]
+        expected_status_code = 400
+        expected_body = load_example("bad-request.json")
+        bad_id = "non-uuid"
+
+        # When
+        response = requests.patch(
+            url=f"{config.BASE_URL}/{config.BASE_PATH}/Appointment/{bad_id}",
+            json=load_example("appointment/id/PATCH-body.json"),
+            headers={
+                "Authorization": f"Bearer {token}",
+                "NHSD-ServiceIdentifier": "NHS0001",
+            },
+        )
+
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
+        assert_that(expected_body).is_equal_to(response.json())
+
+    @pytest.mark.appointment
+    @pytest.mark.integration
+    def test_delete_appointment_bad_id(self, get_token_client_credentials):
+        # Given
+        token = get_token_client_credentials["access_token"]
+        expected_status_code = 400
+        expected_body = load_example("bad-request.json")
+        bad_id = "non-uuid"
+
+        # When
+        response = requests.delete(
+            url=f"{config.BASE_URL}/{config.BASE_PATH}/Appointment/{bad_id}",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "NHSD-ServiceIdentifier": "NHS0001",
+            },
+        )
+
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
+        assert_that(expected_body).is_equal_to(response.json())
