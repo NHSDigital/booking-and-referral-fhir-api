f1d9db70037b174579698b683b63864411676da3
Jalal Hosseini <jalalhosseiny@gmail.com>
2021-09-16 14:45:37 +0100

    AMB-816 add scenario where /Appointment only accept certain id

--- sandbox/src/routes/appointment.py
+++ sandbox/src/routes/appointment.py
@@ -1,8 +1,7 @@
-from fastapi import APIRouter, Header
+from fastapi import APIRouter, Header, Response, status
 from uuid import UUID
 from .examples.example_loader import load_example
 from pydantic import BaseModel
-
 
 route = APIRouter()
 
@@ -16,6 +15,10 @@ class AppointmentBody(BaseModel):
     meta: Profile
 
 
+existing_appointment_id = load_example("appointment/POST-success.txt")
+ENTITY_NOT_FOUND = status.HTTP_403_FORBIDDEN  # Spec is probably wrong and status should be 404
+
+
 @route.get("/Appointment")
 def get_appointment(patientIdentifier: str, NHSD_ServiceIdentifier: str = Header(...)):
     return load_example("appointment/GET-success.json")
@@ -27,24 +30,40 @@ def create_appointment(NHSD_ServiceIdentifier: str = Header(...)):
 
 
 @route.get("/Appointment/{id}")
-def get_appointment_id(id: UUID, NHSD_ServiceIdentifier: str = Header(...)):
-    return load_example("appointment/id/GET-success.json")
+def get_appointment_id(response: Response, id: UUID, NHSD_ServiceIdentifier: str = Header(...)):
+    if str(id) == existing_appointment_id:
+        return load_example("appointment/id/GET-success.json")
+    else:
+        response.status_code = ENTITY_NOT_FOUND
+        return load_example("entity-not-found.json")
 
 
 @route.patch("/Appointment/{id}")
-def patch_appointment_id(
-    body: AppointmentBody, id: UUID, NHSD_ServiceIdentifier: str = Header(...)
-):
-    return ""
+def patch_appointment_id(response: Response,
+                         body: AppointmentBody, id: UUID, NHSD_ServiceIdentifier: str = Header(...)
+                         ):
+    if str(id) == existing_appointment_id:
+        return ""
+    else:
+        response.status_code = ENTITY_NOT_FOUND
+        return load_example("entity-not-found.json")
 
 
 @route.put("/Appointment/{id}")
-def put_appointment_id(
-    body: AppointmentBody, id: UUID, NHSD_ServiceIdentifier: str = Header(...)
-):
-    return ""
+def put_appointment_id(response: Response,
+                       body: AppointmentBody, id: UUID, NHSD_ServiceIdentifier: str = Header(...)
+                       ):
+    if str(id) == existing_appointment_id:
+        return ""
+    else:
+        response.status_code = ENTITY_NOT_FOUND
+        return load_example("entity-not-found.json")
 
 
 @route.delete("/Appointment/{id}")
-def delete_appointment_id(id: UUID, NHSD_ServiceIdentifier: str = Header(...)):
-    return ""
+def delete_appointment_id(response: Response, id: UUID, NHSD_ServiceIdentifier: str = Header(...)):
+    if str(id) == existing_appointment_id:
+        return ""
+    else:
+        response.status_code = ENTITY_NOT_FOUND
+        return load_example("entity-not-found.json")
