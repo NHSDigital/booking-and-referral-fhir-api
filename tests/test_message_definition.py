d95724b01aa78f5fd088246f486a8802839147a2
Jalal Hosseini <jalalhosseiny@gmail.com>
2021-10-14 14:58:15 +0100

    AMB-862 add tests for MessageDefinition endpoint

---
+++ tests/test_message_definition.py
@@ -0,0 +1,29 @@
+import pytest
+import requests
+from .configuration import config
+from assertpy import assert_that
+from .example_loader import load_example
+
+
+class TestMessageDefinition:
+    @pytest.mark.message_definition
+    @pytest.mark.integration
+    @pytest.mark.sandbox
+    def test_get_message_definition(self, get_token_client_credentials):
+        # Given
+        token = get_token_client_credentials["access_token"]
+        expected_status_code = 200
+        expected_body = load_example("message_definition/GET-success.json")
+
+        # When
+        response = requests.get(
+            url=f"{config.BASE_URL}/{config.BASE_PATH}/MessageDefinition",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "NHSD-Service": "NHS0001",
+            },
+        )
+
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
+        assert_that(expected_body).is_equal_to(response.json())
