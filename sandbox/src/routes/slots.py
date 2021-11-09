e4a3b6fbe736edb9d87ce70c6b167be3bdde18f6
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2021-11-09 16:11:25 +0000

    AMB-903 add 405 error to slots

--- sandbox/src/routes/slots.py
+++ sandbox/src/routes/slots.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Header
+from fastapi.responses import JSONResponse
 from enum import Enum
 from datetime import datetime
 from .example_loader import load_example
@@ -31,3 +32,11 @@ def slot(
         NHSD_Token: str = Header(...)
 ):
     return load_example("slots/GET-success.json")
+
+
+@route.post("/Slot")
+@route.put("/Slot")
+@route.patch("/Slot")
+@route.delete("/Slot")
+def method_not_allowed():
+    return JSONResponse(load_example("method-not-allowed.json"), status_code=405)
