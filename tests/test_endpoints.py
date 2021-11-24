fb2885027b8770d76529858b42412b90107172d7
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2021-11-24 11:00:44 +0000

    AMB-903 fix 403 and 402 fault rules

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -30,7 +30,7 @@ class TestEndpoints:
         response = requests.get(
             url=f"{config.BASE_URL}/{config.BASE_PATH}/metadata",
             headers={
-                "Authorization": "Bearer invalid_token",
+                "Authorization": "",
                 "NHSD-Service": "NHS0001",
             },
         )
