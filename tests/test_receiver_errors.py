1575de6a2a9361c3ffe2e796c36b75f010b1d016
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2021-11-26 15:04:32 +0000

    AMB-936 add tests for 401 and 403 recevier errors

---
+++ tests/test_receiver_errors.py
@@ -0,0 +1,55 @@
+import pytest
+import requests
+from .configuration import config
+from assertpy import assert_that
+from .example_loader import load_example
+
+
+class TestReceiverErrors:
+    nhsd_token = "nhsd-token"
+
+    @pytest.mark.errors
+    @pytest.mark.integration
+    @pytest.mark.sandbox
+    def test_401_unauthorized_error(self, get_token_client_credentials):
+        # Given
+        token = get_token_client_credentials["access_token"]
+        expected_status_code = 401
+        expected_body = load_example("unauthorized.json")
+
+        # When
+        response = requests.get(
+            url=f"{config.BASE_URL}/{config.BASE_PATH}/Slot",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "NHSD-Service": "NHS0001-401",
+                "NHSD-Token": self.nhsd_token,
+            },
+        )
+
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
+        assert_that(expected_body).is_equal_to(response.json())
+
+    @pytest.mark.errors
+    @pytest.mark.integration
+    @pytest.mark.sandbox
+    def test_403_forbidden_error(self, get_token_client_credentials):
+        # Given
+        token = get_token_client_credentials["access_token"]
+        expected_status_code = 403
+        expected_body = load_example("forbidden.json")
+
+        # When
+        response = requests.get(
+            url=f"{config.BASE_URL}/{config.BASE_PATH}/Slot",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "NHSD-Service": "NHS0001-403",
+                "NHSD-Token": self.nhsd_token,
+            },
+        )
+
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
+        assert_that(expected_body).is_equal_to(response.json())
