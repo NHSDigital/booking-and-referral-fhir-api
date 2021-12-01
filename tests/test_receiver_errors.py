08051daad3b5938e4b1091cccd83ef3452f43176
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2021-12-01 08:29:26 +0000

    AMB-936 add back rec error tests

--- tests/test_receiver_errors.py
+++ tests/test_receiver_errors.py
@@ -3,9 +3,11 @@ import requests
 from .configuration import config
 from assertpy import assert_that
 from .example_loader import load_example
+from datetime import datetime
 
 
 class TestReceiverErrors:
+    currentTime = datetime.now()
     nhsd_token = "nhsd-token"
 
     @pytest.mark.errors
@@ -24,6 +26,13 @@ class TestReceiverErrors:
                 "Authorization": f"Bearer {token}",
                 "NHSD-Service": "NHS0001-401",
                 "NHSD-Token": self.nhsd_token,
+            },
+            params={
+                "healthcareService": "09a01679-2564-0fb4-5129-aecc81ea2706",
+                "status": ["free"],
+                "start": self.currentTime,
+                "end": self.currentTime,
+                "_include": ["Schedule"],
             },
         )
 
@@ -47,6 +56,103 @@ class TestReceiverErrors:
                 "Authorization": f"Bearer {token}",
                 "NHSD-Service": "NHS0001-403",
                 "NHSD-Token": self.nhsd_token,
+            },
+            params={
+                "healthcareService": "09a01679-2564-0fb4-5129-aecc81ea2706",
+                "status": ["free"],
+                "start": self.currentTime,
+                "end": self.currentTime,
+                "_include": ["Schedule"],
+            },
+        )
+
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
+        assert_that(expected_body).is_equal_to(response.json())
+
+    @pytest.mark.errors
+    @pytest.mark.integration
+    @pytest.mark.sandbox
+    def test_406_not_acceptable_error(self, get_token_client_credentials):
+        # Given
+        token = get_token_client_credentials["access_token"]
+        expected_status_code = 406
+        expected_body = load_example("not-acceptable.json")
+
+        # When
+        response = requests.get(
+            url=f"{config.BASE_URL}/{config.BASE_PATH}/Slot",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "NHSD-Service": "NHS0001-406",
+                "NHSD-Token": self.nhsd_token,
+            },
+            params={
+                "healthcareService": "09a01679-2564-0fb4-5129-aecc81ea2706",
+                "status": ["free"],
+                "start": self.currentTime,
+                "end": self.currentTime,
+                "_include": ["Schedule"],
+            },
+        )
+
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
+        assert_that(expected_body).is_equal_to(response.json())
+
+    @pytest.mark.errors
+    @pytest.mark.integration
+    @pytest.mark.sandbox
+    def test_409_conflict_error(self, get_token_client_credentials):
+        # Given
+        token = get_token_client_credentials["access_token"]
+        expected_status_code = 409
+        expected_body = load_example("conflict.json")
+
+        # When
+        response = requests.get(
+            url=f"{config.BASE_URL}/{config.BASE_PATH}/Slot",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "NHSD-Service": "NHS0001-409",
+                "NHSD-Token": self.nhsd_token,
+            },
+            params={
+                "healthcareService": "09a01679-2564-0fb4-5129-aecc81ea2706",
+                "status": ["free"],
+                "start": self.currentTime,
+                "end": self.currentTime,
+                "_include": ["Schedule"],
+            },
+        )
+
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
+        assert_that(expected_body).is_equal_to(response.json())
+
+    @pytest.mark.errors
+    @pytest.mark.integration
+    @pytest.mark.sandbox
+    def test_422_unprocessable_entity_error(self, get_token_client_credentials):
+        # Given
+        token = get_token_client_credentials["access_token"]
+        expected_status_code = 422
+        expected_body = load_example("unprocessable-entity.json")
+
+        # When
+        response = requests.get(
+            url=f"{config.BASE_URL}/{config.BASE_PATH}/Slot",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "NHSD-Service": "NHS0001-422",
+                "NHSD-Token": self.nhsd_token,
+            },
+            params={
+                "healthcareService": "09a01679-2564-0fb4-5129-aecc81ea2706",
+                "status": ["free"],
+                "start": self.currentTime,
+                "end": self.currentTime,
+                "_include": ["Schedule"],
             },
         )
 
