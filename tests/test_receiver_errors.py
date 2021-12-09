30ead7284b02f04902d109c2e074c4fae4cf0715
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2021-12-09 08:26:47 +0000

    AMB-936 add remaining recevier error tests

--- tests/test_receiver_errors.py
+++ tests/test_receiver_errors.py
@@ -159,3 +159,93 @@ class TestReceiverErrors:
         # Then
         assert_that(expected_status_code).is_equal_to(response.status_code)
         assert_that(expected_body).is_equal_to(response.json())
+
+    @pytest.mark.errors
+    @pytest.mark.integration
+    @pytest.mark.sandbox
+    def test_500_server_error_error(self, get_token_client_credentials):
+        # Given
+        token = get_token_client_credentials["access_token"]
+        expected_status_code = 500
+        expected_body = load_example("server-error.json")
+
+        # When
+        response = requests.get(
+            url=f"{config.BASE_URL}/{config.BASE_PATH}/Slot",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "NHSD-Service": "NHS0001-500",
+                "NHSD-Token": self.nhsd_token,
+            },
+            params={
+                "healthcareService": "09a01679-2564-0fb4-5129-aecc81ea2706",
+                "status": ["free"],
+                "start": self.currentTime,
+                "end": self.currentTime,
+                "_include": ["Schedule"],
+            },
+        )
+
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
+        assert_that(expected_body).is_equal_to(response.json())
+
+    @pytest.mark.errors
+    @pytest.mark.integration
+    @pytest.mark.sandbox
+    def test_501_not_implemented_error(self, get_token_client_credentials):
+        # Given
+        token = get_token_client_credentials["access_token"]
+        expected_status_code = 501
+        expected_body = load_example("not-implemented.json")
+
+        # When
+        response = requests.get(
+            url=f"{config.BASE_URL}/{config.BASE_PATH}/Slot",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "NHSD-Service": "NHS0001-501",
+                "NHSD-Token": self.nhsd_token,
+            },
+            params={
+                "healthcareService": "09a01679-2564-0fb4-5129-aecc81ea2706",
+                "status": ["free"],
+                "start": self.currentTime,
+                "end": self.currentTime,
+                "_include": ["Schedule"],
+            },
+        )
+
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
+        assert_that(expected_body).is_equal_to(response.json())
+
+    @pytest.mark.errors
+    @pytest.mark.integration
+    @pytest.mark.sandbox
+    def test_503_unavailable_error(self, get_token_client_credentials):
+        # Given
+        token = get_token_client_credentials["access_token"]
+        expected_status_code = 503
+        expected_body = load_example("unavailable.json")
+
+        # When
+        response = requests.get(
+            url=f"{config.BASE_URL}/{config.BASE_PATH}/Slot",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "NHSD-Service": "NHS0001-503",
+                "NHSD-Token": self.nhsd_token,
+            },
+            params={
+                "healthcareService": "09a01679-2564-0fb4-5129-aecc81ea2706",
+                "status": ["free"],
+                "start": self.currentTime,
+                "end": self.currentTime,
+                "_include": ["Schedule"],
+            },
+        )
+
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
+        assert_that(expected_body).is_equal_to(response.json())
