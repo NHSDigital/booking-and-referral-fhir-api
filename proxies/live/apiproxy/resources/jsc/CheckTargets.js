3b535ec8bf53d2542a223a5ee18cc109bf00d81c
Andre Silva <silva.andre.daniel@gmail.com>
2021-12-20 12:06:28 +0000

    AMB-960 - create new js policy to check targets

--- proxies/live/apiproxy/resources/jsc/CheckTargets.js
+++ proxies/live/apiproxy/resources/jsc/CheckTargets.js
@@ -1,10 +1,10 @@
 // Get context variables
 var serviceId = context.getVariable("request.header.NHSD-Service");
-var idNotFound = false
+var idNotFound = false;
 
 // Get target url from KVM
 var targetUrl = get_target_url_from_kvm(serviceId)
 if(targetUrl === null | serviceId === null){
-    idNotFound = true
+    idNotFound = true;
 }
 context.setVariable("idNotFound", idNotFound)
