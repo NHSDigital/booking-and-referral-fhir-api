5acf446b6937d8b031e44360684f679efb86c47b
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2022-03-02 10:39:38 +0000

    AMB-1043 use python script for Base64 decoding

--- proxies/live/apiproxy/resources/jsc/DecodeBase64String.js
+++
@@ -1,7 +0,0 @@
-// Define the string
-var encodedStringAtoB = 'SGVsbG8gV29ybGQh';
-
-// Decode the String
-var decodedStringAtoB = atob(encodedStringAtoB);
-
-console.log(decodedStringAtoB);
---
+++ proxies/live/apiproxy/resources/py/DecodeBase64String.py
@@ -0,0 +1,6 @@
+import base64
+
+a = 'eW91ciB0ZXh0'
+decoded = base64.b64decode(a)
+
+print(decoded)
