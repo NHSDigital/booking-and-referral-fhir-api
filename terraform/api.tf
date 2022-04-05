6303b7a9b41214dd202f24027128fde5b9fde713
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-04-05 11:24:26 +0100

    AMB-1059 add apigateway

---
+++ terraform/api.tf
@@ -0,0 +1,63 @@
+resource "aws_api_gateway_rest_api" "main" {
+  name = "${var.app_name}-api"
+}
+
+resource "aws_api_gateway_resource" "main" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_rest_api.main.root_resource_id
+  path_part   = "debug"
+}
+
+resource "aws_api_gateway_method" "main" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.main.id
+  http_method   = "ANY"
+  authorization = "NONE"
+
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+/*
+resource "aws_api_gateway_domain_name" "api_domain_name" {
+  domain_name = "bars.${var.domain_name}"
+  certificate_arn = aws_acm_certificate_validation.cert_validation_root.certificate_arn
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+*/
+
+resource "aws_api_gateway_integration" "main" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  resource_id = aws_api_gateway_resource.main.id
+  http_method = aws_api_gateway_method.main.http_method
+
+  type                    = "MOCK"
+}
+
+resource "aws_api_gateway_deployment" "main" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  stage_name = "${var.environment}-env"
+  depends_on = [aws_api_gateway_integration.main]
+
+  variables = {
+    # just to trigger redeploy on resource changes
+    resources = join(", ", [aws_api_gateway_resource.main.id])
+
+    # note: redeployment might be required with other gateway changes.
+    # when necessary run `terraform taint <this resource's address>`
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+/*
+resource "aws_api_gateway_vpc_link" "this" {
+  name = "${var.app_name}-vpc-link"
+  target_arns = [aws_lb.nlb.arn]
+}
+*/
--- terraform/var.tf
+++ terraform/var.tf
@@ -1,3 +1,9 @@
+variable "app_name" {
+    default = "bars3"
+}
+variable "environment" {
+    default = "dev"
+}
 variable "aws_account_id" {
     description = "AWS account ID"
 }
