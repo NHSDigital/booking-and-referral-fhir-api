154f3a646d9a5a21fd0ee3c34ae2df724ffcba83
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-04-05 11:28:17 +0100

    AMB-1059 add vpc

--- terraform/var.tf
+++ terraform/var.tf
@@ -12,6 +12,41 @@ variable "aws_role_name" {
     description = "AWS role name"
 }
 
+locals {
+    tags = {
+        Project = var.app_name
+        Environment = var.environment
+    }
+}
+
+variable "app_port" {
+    default = 5050
+}
+
 variable "bucket_name" {
     default = "test-bucket-bars"
+}
+variable "cidr" {
+    description = "The CIDR block for the VPC."
+    default     = "10.10.0.0/16"
+}
+
+variable "public_subnets" {
+    description = "List of public subnets"
+    default     = ["10.10.100.0/24", "10.10.101.0/24"]
+}
+
+variable "private_subnets" {
+    description = "List of private subnets"
+    default     = ["10.10.0.0/24", "10.10.1.0/24"]
+}
+
+variable "private_subnets_cidr" {
+    description = "List of private subnets"
+    default     = ["10.10.2.0/24", "10.10.3.0/24"]
+}
+
+variable "availability_zones" {
+    description = "List of availability zones"
+    default     = ["eu-west-2a", "eu-west-2b"]
 }
---
+++ terraform/vpc.tf
@@ -0,0 +1,87 @@
+resource "aws_vpc" "bars_vpc" {
+  cidr_block       = var.cidr
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = merge(local.tags, {Name = "${var.app_name}-${var.environment}-vpc"})
+}
+
+### VPC Network Setup
+
+# Create the private subnets
+resource "aws_subnet" "private_subnets" {
+  count = length(var.private_subnets)
+  vpc_id            = aws_vpc.bars_vpc.id
+  cidr_block = element(var.private_subnets_cidr, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+
+  tags = merge(local.tags, {Name = "${var.app_name}-${var.environment}-private-subnet-${count.index}"})
+}
+
+### Security Group Setup
+
+# ALB Security group
+resource "aws_security_group" "lb" {
+  name        = "${var.app_name}-${var.environment}-lb-sg"
+  vpc_id      = aws_vpc.bars_vpc.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Traffic to the ECS Cluster should only come from the ALB
+# or AWS services through an AWS PrivateLink
+resource "aws_security_group" "ecs_tasks" {
+  name        = "${var.app_name}-${var.environment}-ecs-sg"
+  vpc_id      = aws_vpc.bars_vpc.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = var.app_port
+    to_port     = var.app_port
+    cidr_blocks = [var.cidr]
+  }
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = 443
+    to_port         = 443
+    cidr_blocks = [var.cidr]
+  }
+
+/*
+  egress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    prefix_list_ids = [
+      aws_vpc_endpoint.s3.prefix_list_id
+    ]
+  }
+*/
+
+  egress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks = [var.cidr]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
