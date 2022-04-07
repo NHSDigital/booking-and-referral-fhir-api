60963bf5ee97800aab6ba046e1df1b6ca251b866
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-04-07 17:25:42 +0100

    AMB-1098 use sandbox docker in lambda add ecr and push sandbox and use it in lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,18 +1,10 @@
-data "archive_file" "lambda_debug" {
-  type = "zip"
-  #  source_dir = "../mock-receiver/debug"
-  source_file = "../mock-receiver/debug/index.js"
-  output_path = "build/index.zip"
+resource "aws_lambda_function" "debug_endpoint_function" {
+  function_name = "${local.name_prefix}-debug"
+  image_uri     = "${aws_ecr_repository.bars-ecr-repository.repository_url}@${data.aws_ecr_image.lambda_image.id}"
+  package_type  = "Image"
+  role          = aws_iam_role.iam_for_lambda.arn
 }
 
-resource "aws_lambda_function" "debug_endpoint_function" {
-  function_name    = "${local.name_prefix}-debug"
-  filename         = data.archive_file.lambda_debug.output_path
-  handler          = "index.handler"
-  source_code_hash = data.archive_file.lambda_debug.output_base64sha256
-  runtime          = "nodejs14.x"
-  role             = aws_iam_role.iam_for_lambda.arn
-}
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
