24d048030e41ab596d05e6e0b76edda9f3abd6a8
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2022-04-20 09:30:12 +0100

    AMB-1048 update errors.py with new header

--- sandbox/src/routes/errors.py
+++ sandbox/src/routes/errors.py
@@ -6,29 +6,29 @@ route = APIRouter()
 
 
 @route.get("/errors/{path}")
-def errors(NHSD_Service: str = Header(...)):
-    if str(NHSD_Service) == "NHS0001-401":
+def errors(NHSD_Target_Identifier: str = Header(..., alias="NHSD-Target-Identifier")):
+    if str(NHSD_Target_Identifier) == "NHS0001-401":
         response = load_example("unauthorized.json")
         status_code = 401
-    if str(NHSD_Service) == "NHS0001-403":
+    if str(NHSD_Target_Identifier) == "NHS0001-403":
         response = load_example("forbidden.json")
         status_code = 403
-    if str(NHSD_Service) == "NHS0001-406":
+    if str(NHSD_Target_Identifier) == "NHS0001-406":
         response = load_example("not-acceptable.json")
         status_code = 406
-    if str(NHSD_Service) == "NHS0001-409":
+    if str(NHSD_Target_Identifier) == "NHS0001-409":
         response = load_example("conflict.json")
         status_code = 409
-    if str(NHSD_Service) == "NHS0001-422":
+    if str(NHSD_Target_Identifier) == "NHS0001-422":
         response = load_example("unprocessable-entity.json")
         status_code = 422
-    if str(NHSD_Service) == "NHS0001-500":
+    if str(NHSD_Target_Identifier) == "NHS0001-500":
         response = load_example("server-error.json")
         status_code = 500
-    if str(NHSD_Service) == "NHS0001-501":
+    if str(NHSD_Target_Identifier) == "NHS0001-501":
         response = load_example("not-implemented.json")
         status_code = 501
-    if str(NHSD_Service) == "NHS0001-503":
+    if str(NHSD_Target_Identifier) == "NHS0001-503":
         response = load_example("unavailable.json")
         status_code = 503
 
