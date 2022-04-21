61aebf733de16bb401a8b8f8ce8b315c691e2c63
Andre Silva <silva.andre.daniel@gmail.com>
2022-04-21 16:05:03 +0100

    remove unnecessary comment

--- terraform/api.tf
+++ terraform/api.tf
@@ -6,7 +6,7 @@ resource "aws_apigatewayv2_api" "service_api" {
 }
 
 locals {
-  # NHSD server cert file
+  # NHSD cert file
   truststore_file_name = "truststore.crt"
 }
 resource "aws_s3_bucket" "truststore_bucket" {
