af47b653f34d32371207f7eabd56fced4b5ecae4
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-04-26 16:33:10 +0100

    AMB-1084 add dependabot for sandbox and use localhost instead of 0.0.0.0

--- sandbox/src/main.py
+++ sandbox/src/main.py
@@ -4,6 +4,7 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from starlette.responses import Response
 from starlette.status import HTTP_200_OK
+
 from routes import (
     errors,
     slots,
@@ -66,4 +67,4 @@ def status():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=9000)
+    uvicorn.run(app, host="127.0.0.1", port=9000)
