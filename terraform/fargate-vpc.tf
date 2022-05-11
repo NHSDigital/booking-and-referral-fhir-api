e510b38530c8e446a0c03ffa58bf90de17c03fa9
Sirisha Bajanki <sirisha.bajanki1@nhs.net>
2022-05-11 14:20:16 +0100

    changed vpc

--- terraform/fargate-vpc.tf
+++ terraform/fargate-vpc.tf
@@ -1,17 +1,14 @@
-resource "aws_vpc" "fargate-vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    "Name" = local.name_prefix
-     }
-
+data "aws_vpc" "fargate-vpc" {
+  id = "vpc-05a0c2d68352a2ef4"
 }
+
 locals {
   container_port = 9000
 }
 
 resource "aws_security_group" "ecs_tasks" {
   name   = "${local.name_prefix}-sg-task"
-  vpc_id = aws_vpc.fargate-vpc.id
+  vpc_id = data.aws_vpc.fargate-vpc.id
  
   ingress {
    protocol         = "tcp"
@@ -31,10 +28,58 @@ resource "aws_security_group" "ecs_tasks" {
 }
 
 resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.fargate-vpc.id
+  vpc_id     = data.aws_vpc.fargate-vpc.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
     Name = local.name_prefix
   }
+}  
+
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id       = data.aws_vpc.fargate-vpc.id
+  service_name = "com.amazonaws.${var.region}.ecr.dkr"
+  vpc_endpoint_type = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = [aws_subnet.main.id]
+security_group_ids = [
+    "${aws_security_group.ecs_tasks.id}",
+  ]
+tags = {
+    Name = "${local.name_prefix}-ecr"
+    
+  }
+
+
+}
+
+resource "aws_vpc_endpoint" "cloudwatch_logs" {
+  vpc_id       = data.aws_vpc.fargate-vpc.id
+  service_name = "com.amazonaws.${var.region}.logs"
+  vpc_endpoint_type = "Interface"
+  subnet_ids          = [aws_subnet.main.id]
+security_group_ids = [
+    "${aws_security_group.ecs_tasks.id}",
+  ]
+tags = {
+    Name = "${local.name_prefix}-logs"
+    
+  }
+
+
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = data.aws_vpc.fargate-vpc.id
+  service_name = "com.amazonaws.${var.region}.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = ["rtb-0618565988622142e"]
+
+tags = {
+    Name = "${local.name_prefix}-s3"
+    
+  }
+
+
 }
--- terraform/fargate.tf
+++ terraform/fargate.tf
@@ -6,20 +6,20 @@ resource "aws_ecs_cluster" "ecs-cluster" {
     value = "enabled"
   }
 
-    configuration {
-    execute_command_configuration {
-      logging    = "OVERRIDE"
+    # configuration {
+    # execute_command_configuration {
+    #   logging    = "OVERRIDE"
       
-      log_configuration {
-        cloud_watch_log_group_name     = aws_cloudwatch_log_group.cluster_container_logs.name
-      }
-    }
-  }
+    #   log_configuration {
+    #     cloud_watch_log_group_name     = aws_cloudwatch_log_group.cluster_container_logs.name
+    #   }
+    # }
+  #}
 }
 
-resource "aws_cloudwatch_log_group" "cluster_container_logs" {
-  name = "${local.name_prefix}-cluster"
-}
+# resource "aws_cloudwatch_log_group" "cluster_container_logs" {
+#   name = "${local.name_prefix}-cluster"
+# }
 
 
 resource "aws_iam_role" "ecs-task-role" {
@@ -66,7 +66,7 @@ resource "aws_ecs_task_definition" "mock-receiver" {
   ])
 }
 
-resource "aws_ecs_service" "mock-receiver-service" {-
+resource "aws_ecs_service" "mock-receiver-service" {
  
   name            = "${local.name_prefix}-service"
   cluster         = aws_ecs_cluster.ecs-cluster.id
