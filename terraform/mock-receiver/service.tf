caf484754d92a6d72cc10267d043acc9f7aeef54
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-06-09 21:34:18 +0100

    AMB-1667 force deploy aws fargate service

--- terraform/mock-receiver/service.tf
+++ terraform/mock-receiver/service.tf
@@ -1,12 +1,15 @@
 resource "aws_ecs_service" "mock-receiver-service" {
-  name             = var.name_prefix
-  cluster          = var.cluster_id
-  task_definition  = aws_ecs_task_definition.mock-receiver.arn
-  platform_version = "1.4.0"
+  name                               = var.name_prefix
+  cluster                            = var.cluster_id
+  task_definition                    = aws_ecs_task_definition.mock-receiver.arn
+  platform_version                   = "1.4.0"
   # 1.3.0 doesn't need a vpc_endpoint to *.ecr.api see:https://docs.aws.amazon.com/AmazonECR/latest/userguide/vpc-endpoints.html
-  desired_count = 0
+  desired_count                      = 2
   // TODO: When tightening security group keep this to 3. It'll deploy each task into a subnet (in each availability zone). When modifying security group make sure all three are still running. This is especially important when changing the cidr blocks
-  launch_type      = "FARGATE"
+  launch_type                        = "FARGATE"
+  force_new_deployment               = true
+  deployment_maximum_percent         = 100
+  deployment_minimum_healthy_percent = 0
   network_configuration {
     security_groups  = [aws_security_group.service_security_group.id]
     subnets          = var.subnet_ids
@@ -14,7 +17,7 @@ resource "aws_ecs_service" "mock-receiver-service" {
   }
   load_balancer {
     target_group_arn = var.alb_tg_arn
-      container_name = local.service_name
+    container_name   = local.service_name
     container_port   = var.container_port
   }
 }
