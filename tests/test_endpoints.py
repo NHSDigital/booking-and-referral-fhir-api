a2c444bda092e919fa711e1a6169495c0e137aa5
Jack McCaffrey <jack.mccaffrey1@nhs.net>
2022-07-29 12:49:02 +0100

    AMB-1230: Removed duplicate test

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -69,7 +69,7 @@ class TestEndpoints:
     @pytest.mark.broker
     def test_invalid_nhsd_service_identifier(self, get_token_client_credentials):
         """
-          This test checks a request with an invalid service identifier
+          This test checks a request with an invalid service identifier returns a 404 error.
         """
         # Given
         token = get_token_client_credentials["access_token"]
@@ -89,29 +89,6 @@ class TestEndpoints:
         )
         # Then
         assert_that(expected_status_code).is_equal_to(response.status_code)
-
-    def test_nonexistant_target_identifier_returns_proxy_404(self, get_token_client_credentials):
-        """
-
-        """
-        token = get_token_client_credentials["access_token"]
-        expected_status_code = 404
-        target_identifier = json.dumps({"value": "invalid", "system": "tests"})
-        target_identifier_encoded = base64.b64encode(bytes(target_identifier, "utf-8"))
-
-        # When
-        response = requests.get(
-            url=f"{config.BASE_URL}/{config.BASE_PATH}/Appointment",
-            headers={
-                "Authorization": f"Bearer {token}",
-                "NHSD-Target-Identifier": target_identifier_encoded,
-                "X-Request-Id": "c1ab3fba-6bae-4ba4-b257-5a87c44d4a91",
-                "X-Correlation-Id": "9562466f-c982-4bd5-bb0e-255e9f5e6689",
-            },
-        )
-        # Then
-        assert_that(expected_status_code).is_equal_to(response.status_code)
-        pass
 
     @pytest.mark.integration
     @pytest.mark.sandbox
