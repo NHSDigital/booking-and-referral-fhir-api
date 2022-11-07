9ff492aaf8287a0f648b6f66d5ed2a2f2ad4c9c1
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2022-11-07 13:35:27 +0000

    AMB-1319 add test_get_metadata_capability_statement

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -70,3 +70,29 @@ class TestMetadata:
         # Then
         assert_that(expected_status_code).is_equal_to(response.status_code)
         assert_that(expected_body).is_equal_to(response.json())
+
+    @pytest.mark.metadata
+    @pytest.mark.integration
+    @pytest.mark.sandbox
+    def test_get_metadata_capability_statement(self, get_token_client_credentials):
+        """
+          test to get metadata capability statement
+        """
+        # Given
+        token = get_token_client_credentials["access_token"]
+        expected_status_code = 200
+        expected_body = load_example("metadata/BaRS_API_Capability_Statement .json")
+
+        # When
+        response = requests.get(
+            url=f"{config.BASE_URL}/{config.BASE_PATH}/metadata",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "X-Request-Id": "c1ab3fba-6bae-4ba4-b257-5a87c44d4a91",
+                "X-Correlation-Id": "9562466f-c982-4bd5-bb0e-255e9f5e6689"
+            },
+        )
+
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
+        assert_that(expected_body).is_equal_to(response.json())
