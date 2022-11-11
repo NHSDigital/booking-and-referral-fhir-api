ac160262b700be36498b620ad4f4954e3b2e9ed9
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-11-11 14:09:40 +0000

    AMB-1325 raise fault if id is malformed and assign response

--- proxies/live/apiproxy/resources/jsc/CheckTargets.js
+++ proxies/live/apiproxy/resources/jsc/CheckTargets.js
@@ -1,18 +1,21 @@
 // Get context variables
-var b64decoded = JSON.parse(context.getVariable("b64decoded"));
+var b64decoded = JSON.parse(context.getVariable('b64decoded'))
 
-
-var idNotFound = false;
+// Check the structure of the TargetId
+if (b64decoded != null && b64decoded['system'] && b64decoded['value']) {
+  var idNotFound = false
 
 // Get target url from KVM (NHSD-Target-Identifier header)
-if(b64decoded != null){
-    var system = b64decoded.system
-    var value = b64decoded.value
-    var targetUrl = get_target_url_from_kvm_nhsd_target_identifier(system, value)
-    if(targetUrl === null | system === null | value === null){
-        idNotFound = true;
-    }
+  var system = b64decoded.system
+  var value = b64decoded.value
+  var targetUrl = get_target_url_from_kvm_nhsd_target_identifier(system, value)
+  if (targetUrl === null) {
+    idNotFound = true
+  }
+
+  context.setVariable('idNotFound', idNotFound)
+
+} else {
+  context.setVariable('idMalformed', true)
 }
 
-
-context.setVariable("idNotFound", idNotFound)
--- tests/test_target_identifier.py
+++ tests/test_target_identifier.py
@@ -40,8 +40,7 @@ class TestProcessMessage:
 
     @pytest.mark.target_identifier
     @pytest.mark.broker
-    @pytest.mark.debug
-    def test_invalid_nhsd_service_identifier(self, get_token_client_credentials):
+    def test_invalid_nhsd_service_identifier_value(self, get_token_client_credentials):
         """
           This test checks a request with an invalid service identifier returns a 404 error.
         """
@@ -50,6 +49,34 @@ class TestProcessMessage:
         expected_status_code = 404
         expected_body = load_example("proxy-not-found.json")
         target_identifier = json.dumps({"value": "invalid", "system": "tests"})
+        target_identifier_encoded = base64.b64encode(bytes(target_identifier, "utf-8"))
+
+        # When
+        response = requests.get(
+            url=f"{config.BASE_URL}/{config.BASE_PATH}/metadata",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "NHSD-Target-Identifier": target_identifier_encoded,
+                "X-Request-Id": "c1ab3fba-6bae-4ba4-b257-5a87c44d4a91",
+                "X-Correlation-Id": "9562466f-c982-4bd5-bb0e-255e9f5e6689",
+            },
+        )
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
+        assert_that(expected_body).is_equal_to(response.json())
+
+    @pytest.mark.target_identifier
+    @pytest.mark.broker
+    @pytest.mark.debug
+    def test_malformed_nhsd_service_identifier(self, get_token_client_credentials):
+        """
+          This test checks a request with an invalid service identifier returns a 404 error.
+        """
+        # Given
+        token = get_token_client_credentials["access_token"]
+        expected_status_code = 400
+        expected_body = load_example("proxy-not-found.json")
+        target_identifier = json.dumps({"system": "tests"})
         target_identifier_encoded = base64.b64encode(bytes(target_identifier, "utf-8"))
 
         # When
