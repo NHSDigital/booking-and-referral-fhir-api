724e24ab57758c1496d088662fcc723ea7f5059e
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-11-11 13:45:54 +0000

    AMB-1325 add policies for the structure of the identifier header

--- proxies/live/apiproxy/resources/py/DecodeBase64String.py
+++ proxies/live/apiproxy/resources/py/DecodeBase64String.py
@@ -1,10 +1,6 @@
 import base64
-import binascii
 
 b64encoded = flow.getVariable("request.header.NHSD-Target-Identifier")
-try:
-    b64decoded = base64.b64decode(b64encoded)
-    flow.setVariable("b64decoded", b64decoded)
-except binascii.Error:
-    flow.setVariable("b64decodedFailed", True)
+b64decoded = base64.b64decode(b64encoded)
 
+flow.setVariable("b64decoded", b64decoded)
--- tests/test_target_identifier.py
+++ tests/test_target_identifier.py
@@ -14,12 +14,33 @@ class TestProcessMessage:
 
     @pytest.mark.target_identifier
     @pytest.mark.broker
-    @pytest.mark.debug
-    def test_create_process_message(self, get_token_client_credentials):
-        pass
+    def test_invalid_base64_encoded_identifier(self, get_token_client_credentials):
+        """
+          This test checks a request with an invalid encoded service identifier returns a 400 error.
+        """
+        # Given
+        token = get_token_client_credentials["access_token"]
+        expected_status_code = 400
+        expected_body = load_example("invalid-base64.json")
+        target_identifier_encoded = "this-is-not-base-64"
+
+        # When
+        response = requests.get(
+            url=f"{config.BASE_URL}/{config.BASE_PATH}/metadata",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "NHSD-Target-Identifier": target_identifier_encoded,
+                "X-Request-Id": "c1ab3fba-6bae-4ba4-b257-5a87c44d4a91",
+                "X-Correlation-Id": "9562466f-c982-4bd5-bb0e-255e9f5e6689",
+            },
+        )
+        # Then
+        assert_that(expected_status_code).is_equal_to(response.status_code)
+        assert_that(expected_body).is_equal_to(response.json())
 
     @pytest.mark.target_identifier
     @pytest.mark.broker
+    @pytest.mark.debug
     def test_invalid_nhsd_service_identifier(self, get_token_client_credentials):
         """
           This test checks a request with an invalid service identifier returns a 404 error.
