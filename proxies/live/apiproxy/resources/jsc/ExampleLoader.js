9659743d4cc351fb47dd902bb721a4f2d3273dcc
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2022-11-15 10:35:38 +0000

    AMB-1333 use JS policy to read example file

---
+++ proxies/live/apiproxy/resources/jsc/ExampleLoader.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+  
+let rawdata = fs.readFileSync('../examples/metadata/BaRS_API_Capability_Statement .json')
+let data = JSON.parse(rawdata);
+
+context.setVariable("data", data);
