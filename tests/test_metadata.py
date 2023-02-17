c77e7ef418fe37490ada8260ecb17afed0aeb064
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2023-02-17 09:01:19 +0000

    AMB-9990 remove redundant test

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -15,35 +15,6 @@ class TestMetadata:
     @pytest.mark.metadata
     @pytest.mark.integration
     @pytest.mark.sandbox
-    def test_get_metadata(self, get_token_client_credentials):
-        """
-          test to get metadata for the target identifier
-        """
-        # Given
-        token = get_token_client_credentials["access_token"]
-        expected_status_code = 200
-        expected_body = load_example("metadata/GET-success.json")
-        target_identifier = json.dumps({"value": self.target_id, "system": "tests"})
-        target_identifier_encoded = base64.b64encode(bytes(target_identifier, "utf-8"))
-
-        # When
-        response = requests.get(
-            url=f"{config.BASE_URL}/{config.BASE_PATH}/metadata",
-            headers={
-                "Authorization": f"Bearer {token}",
-                "NHSD-Target-Identifier": target_identifier_encoded,
-                "X-Request-Id": "c1ab3fba-6bae-4ba4-b257-5a87c44d4a91",
-                "X-Correlation-Id": "9562466f-c982-4bd5-bb0e-255e9f5e6689"
-            },
-        )
-
-        # Then
-        assert_that(expected_status_code).is_equal_to(response.status_code)
-        assert_that(expected_body).is_equal_to(response.json())
-
-    @pytest.mark.metadata
-    @pytest.mark.integration
-    @pytest.mark.sandbox
     def test_metadata_method_not_allowed(self, get_token_client_credentials):
         """
           test to posting metadata for the target identifier
@@ -82,12 +53,15 @@ class TestMetadata:
         token = get_token_client_credentials["access_token"]
         expected_status_code = 200
         expected_body = load_example("metadata/BaRS_API_Capability_Statement.json")
+        target_identifier = json.dumps({"value": self.target_id, "system": "tests"})
+        target_identifier_encoded = base64.b64encode(bytes(target_identifier, "utf-8"))
 
         # When
         response = requests.get(
             url=f"{config.BASE_URL}/{config.BASE_PATH}/metadata",
             headers={
                 "Authorization": f"Bearer {token}",
+                "NHSD-Target-Identifier": target_identifier_encoded,
                 "X-Request-Id": "c1ab3fba-6bae-4ba4-b257-5a87c44d4a91",
                 "X-Correlation-Id": "9562466f-c982-4bd5-bb0e-255e9f5e6689"
             },
