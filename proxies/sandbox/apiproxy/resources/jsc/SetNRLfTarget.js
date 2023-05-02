b3195c7f3fc77931826fba02a1b934960552a86b
Jalal Hosseini <jalalhosseiny@gmail.com>
2023-05-02 17:54:01 +0100

    AMB-1504 fix live and add nrlf target to the sandbox

--- proxies/sandbox/apiproxy/resources/jsc/SetNRLfTarget.js
+++
@@ -1,5 +0,0 @@
-// Override target url
-context.setVariable(
-    "target.url",
-    "https://sandbox.api.service.nhs.uk/record-locator/consumer/FHIR/R4/DocumentReference?subject:identifier=" + context.getVariable("query_params.subjectIdentifier")
-)
