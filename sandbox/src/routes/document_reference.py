a7ef73fb05976a50530c2ffc046cd8845b4dfbf9
Jalal Hosseini <jalalhosseiny@gmail.com>
2023-05-11 15:56:53 +0100

    AMB-1504 add sandbox logic for /DocumentReference

---
+++ sandbox/src/routes/document_reference.py
@@ -0,0 +1,21 @@
+from urllib.parse import parse_qs
+
+from fastapi import APIRouter, Request
+
+from .example_loader import load_example
+
+route = APIRouter()
+
+
+@route.get("/DocumentReference")
+def get_document_reference(request: Request):
+    q_params = parse_qs(request.url.query)
+    subject_ids = q_params.get("subject:identifier")
+    if not subject_ids or len(subject_ids) == 0:
+        return load_example("bad-request.json")
+
+    if subject_ids[0] == "https://fhir.nhs.uk/Id/nhs-number|4409815415":
+        return load_example("document_reference/GET-success-empty.json")
+    else:
+        return load_example("document_reference/GET-success.json")
+
