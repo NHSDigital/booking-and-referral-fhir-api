2ab9ddbeb718f7a46fb07aff9f2711ec751b4644
Jalal Hosseini <jalalhosseiny@gmail.com>
2023-05-11 15:29:16 +0100

    AMB-1504 add empty bundle for /DocumentReference testing

--- tests/test_document_reference.py
+++ tests/test_document_reference.py
@@ -27,7 +27,7 @@ class TestDocumentReference:
         # Given
         token = get_token_client_credentials["access_token"]
         expected_status_code = 200
-        expected_body = load_example("document_reference/GET-success.json")
+        expected_body = load_example("document_reference/GET-success-empty.json")
 
         # When
         response = requests.get(
