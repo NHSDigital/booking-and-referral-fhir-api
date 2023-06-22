e136cfeb40384bed8207047145aba86dc5358fd3
artronics <jalalhosseiny@gmail.com>
2023-06-22 18:59:28 +0100

    AMB-1558 enforce target-id for /metadata as required from AMB-1455

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -61,6 +61,7 @@ class TestMetadata:
             url=f"{config.BASE_URL}/{config.BASE_PATH}/metadata",
             headers={
                 "Authorization": f"Bearer {token}",
+                "NHSD-Target-Identifier": self.target_id_encoded,
                 "X-Request-Id": "c1ab3fba-6bae-4ba4-b257-5a87c44d4a91",
                 "X-Correlation-Id": "9562466f-c982-4bd5-bb0e-255e9f5e6689",
                 "NHSD-End-User-Organisation": "test",
