a702d5d9b0335a6e1fad441e4a435245af331258
almas-liaqat <Almas.liaqat@accentre.com>
2023-07-07 15:51:50 +0100

    updated Set & Remove version logic

---
+++ proxies/sandbox/apiproxy/resources/jsc/SetnRemoveVersion.js
@@ -0,0 +1,20 @@
+var acceptHeader = context.getVariable('request.header.Accept');
+var acceptArray = acceptHeader.split(';');
+var versionNumber = '1.0.0';
+var modifiedAcceptHeader ='';
+
+for (var i = 0; i < acceptArray.length; i++) {
+    var acceptValue = acceptArray[i].trim();
+     if (acceptValue.startsWith('version=')) {
+            versionNumber = acceptValue.split('=')[1];  //Getting versionNumber after '=' sign
+            versionNumber = versionNumber.split('-')[0];  //Removing beta/aplha from end
+    }
+    else if (acceptValue.startsWith('application'))
+    {
+        modifiedAcceptHeader=acceptValue
+    }
+}
+
+context.setVariable('versionNumber', versionNumber);
+
+context.setVariable('request.header.Accept', modifiedAcceptHeader);
