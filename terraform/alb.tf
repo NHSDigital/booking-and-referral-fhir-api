47cb5b8aba5aef3e39cd41252f450a75be16b955
ewan-IW <ewan.childs@infinityworks.com>
2023-07-11 16:54:34 +0100

    Changed to private subnet_ids

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -2,11 +2,11 @@ module "alb" {
   source = "./alb"
 
   name_prefix          = local.name_prefix
-  short_prefix          = local.short_prefix
+  short_prefix         = local.short_prefix
   container_port       = var.container_port
   infra_private_subnet = local.private_subnet_cidr
   infra_public_subnet  = local.public_subnet_cidr
   listener_port        = 80
-  public_subnet_ids    = local.public_subnet_ids
+  private_subnet_ids    = local.private_subnet_ids
   vpc_id               = local.vpc_id
 }
