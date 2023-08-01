bc0c7e62c132fe86773150a198b74e59c2bd50b4
Jalal Hosseini <jalalhosseiny@gmail.com>
2023-08-01 21:38:18 +0100

    AMB-1564 fix validation

--- proxies/live/apiproxy/resources/jsc/ValidateRequest.js
+++ proxies/live/apiproxy/resources/jsc/ValidateRequest.js
@@ -1,31 +1,33 @@
-function requiredHeaders(context) {
-    const hasHeader = (name) => {
-        const headers = context.getVariable("request.headers.name")
-        return headers
-            .map(header => header.toLowerCase())
-            .some(header => header === name)
-    }
+function hasRequiredHeaders(context) {
+    const headers = context.getVariable("request.headers.names").toArray()
 
+    const hasHeader = (name) => {
+        for (var i = 0; i < headers.length; i++) {
+            if (String(headers[i]).toLowerCase().trim() === name.toLowerCase().trim()) return true
+        }
+        return false
+    }
     return hasHeader("x-request-id") && hasHeader("x-correlation-id")
 }
 
-function apiVersion(context) {
+function isVersionValid(context) {
     const path = context.getVariable("proxy.pathsuffix")
     if (path.startsWith("/DocumentReference")) {
         const version = context.getVariable("versionNumber")
         return version === "1.1.0"
     }
+    return true
 }
 
 function validate(context) {
-    if (!requiredHeaders()) {
+    if (!hasRequiredHeaders(context)) {
         return {
             name: "400InvalidHeaders",
             statusCode: 400
 
         }
     }
-    if (!apiVersion(context)) {
+    if (!isVersionValid(context)) {
         return {
             name: "406PageNotAcceptable",
             statusCode: 406
