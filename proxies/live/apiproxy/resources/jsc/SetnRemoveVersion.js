65a1f727668171476ce00b8929c08842dea1a29d
RyanThomas1214 <ryan_j_thomas@hotmail.co.uk>
2023-08-09 12:29:43 +0100

    AMB-1672 remove modified Accept header

--- proxies/live/apiproxy/resources/jsc/SetnRemoveVersion.js
+++ proxies/live/apiproxy/resources/jsc/SetnRemoveVersion.js
@@ -1,18 +1,11 @@
 var acceptHeader = context.getVariable('request.header.Accept');
 var acceptArray = acceptHeader.split(';');
 var versionNumber = '1.0.0';
-var modifiedAcceptHeader ='';
 
 for (var i = 0; i < acceptArray.length; i++) {
     var acceptValue = acceptArray[i].trim();
-     if (acceptValue.startsWith('version=')) {
-            versionNumber = acceptValue.split('=')[1];  //Getting versionNumber after '=' sign
-            versionNumber = versionNumber.split('-')[0];  //Removing beta/aplha from end
-    }
-    else if (acceptValue.startsWith('application'))
-    {
-        modifiedAcceptHeader=acceptValue
-    }
+    versionNumber = acceptValue.split('=')[1];  //Getting versionNumber after '=' sign
+    versionNumber = versionNumber.split('-')[0];  //Removing beta/aplha from end
 }
 
 context.setVariable('versionNumber', versionNumber);
