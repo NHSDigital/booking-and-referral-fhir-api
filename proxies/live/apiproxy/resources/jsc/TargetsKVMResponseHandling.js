12fe1278ac9cb2283bb26c36866e6175004f755b
ewan-IW <ewan.childs@infinityworks.com>
2023-10-16 12:50:18 +0100

    removing unsused JS plicies, updating assignMessage policy and GetTargetsKVM

--- proxies/live/apiproxy/resources/jsc/TargetsKVMResponseHandling.js
+++
@@ -1,7 +0,0 @@
-var responseBody = response.body.asJson;
-
-if (responseBody && responseBody.targets) {
-    var extractedTarget = responseBody.targets;
-
-    context.setVariable("myExtractedURL", extractedTarget);
-}
