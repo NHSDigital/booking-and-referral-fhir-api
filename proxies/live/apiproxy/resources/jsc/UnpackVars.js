db45d6ac3f2aad60ba5288721e0208128a3304a3
ewan-IW <ewan.childs@infinityworks.com>
2023-10-18 09:13:27 +0100

    Aligning code with BaRS before PR

--- proxies/live/apiproxy/resources/jsc/UnpackVars.js
+++ proxies/live/apiproxy/resources/jsc/UnpackVars.js
@@ -1,3 +1,3 @@
 
 var bookingReferralConfig =  JSON.parse(context.getVariable("targets"));
-set_variables("booking-and-referral-config", bookingReferralConfig)
+set_variables("booking-and-referral-config", bookingReferralConfig);
