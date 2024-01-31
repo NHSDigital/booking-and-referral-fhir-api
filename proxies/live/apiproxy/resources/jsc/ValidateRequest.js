f17668fa31983bf881261331ac0807e177a4a3d0
ewan-IW <ewan.childs@infinityworks.com>
2024-01-31 15:13:11 +0000

    Removing suffix validation from ValidateRequest.js

--- proxies/live/apiproxy/resources/jsc/ValidateRequest.js
+++ proxies/live/apiproxy/resources/jsc/ValidateRequest.js
@@ -1,14 +1,14 @@
-function validEndpoint(context) {
-    const path = context.getVariable("proxy.pathsuffix")
-    return path.startsWith("/$process-message") ||
-        path.startsWith("/Slot") ||
-        path.startsWith("/metadata") ||
-        path.startsWith("/Appointment") ||
-        path.startsWith("/ServiceRequest") ||
-        path.startsWith("/registry") ||
-        path.startsWith("/DocumentReference") ||
-        path.startsWith("/MessageDefinition")
-}
+// function validEndpoint(context) {
+//     const path = context.getVariable("proxy.pathsuffix")
+//     return path.startsWith("/$process-message") ||
+//         path.startsWith("/Slot") ||
+//         path.startsWith("/metadata") ||
+//         path.startsWith("/Appointment") ||
+//         path.startsWith("/ServiceRequest") ||
+//         path.startsWith("/registry") ||
+//         path.startsWith("/DocumentReference") ||
+//         path.startsWith("/MessageDefinition")
+// }
 
 function hasRequiredHeaders(context) {
     const headers = context.getVariable("request.headers.names").toArray()
@@ -32,13 +32,13 @@ function isVersionValid(context) {
 }
 
 function validate(context) {
-    if (!validEndpoint(context)) {
-        return {
-            name: "404PageNotFound",
-            statusCode: 404
+    // if (!validEndpoint(context)) {
+    //     return {
+    //         name: "404PageNotFound",
+    //         statusCode: 404
 
-        }
-    }
+    //     }
+    // }
     if (!hasRequiredHeaders(context)) {
         return {
             name: "400InvalidHeaders",
