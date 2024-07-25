9b4be8e457ae4f1811258989119fc5622e023941
Valswyn-NHS <valswyn.alfonso1@nhs.net>
2024-07-25 18:46:14 +0100

    Initial commit

--- infra/cert_storage.tf
+++ infra/cert_storage.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "cert_storage" {
 
 locals {
   # NHSD cert file
-  truststore_file_name = "nhs_truststore.crt"
+  truststore_file_name = "nhs_truststore.pem"
 }
 
 data "aws_s3_object" "cert" {
--- terraform/api/truststore.tf
+++ terraform/api/truststore.tf
@@ -1,6 +1,6 @@
 locals {
   # NHSD cert file
-  truststore_file_name = "nhs_truststore.crt"
+  truststore_file_name = "nhs_truststore.pem"
 }
 
 resource "aws_s3_bucket" "truststore_bucket" {
